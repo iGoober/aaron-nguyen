b769af57d98754fa1108ada43c8da04fcca377f6
Aaron Nguyen <nguyengochien3@gmail.com>
2023-08-22 14:15:02 -0700

    stuff

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+    return (
+        <header>
+            <nav>
+                <a href="/">
+                    <img src ="https://static.wikia.nocookie.net/majo-no-tabitabi/images/f/f1/ElainaViolent.jpg/revision/latest?cb=20210325134307"></img>
+                </a>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav
