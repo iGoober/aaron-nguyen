a0708ef8ef22edc3fe53b42fd6b5b2e3050c4bdf
Aaron Nguyen <nguyengochien3@gmail.com>
2023-08-22 22:28:00 -0700

    working..

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import img1 from './assets/Beach.jpg'
 import img2 from './assets/IMG_5847 16.jpg'
 import img3 from './assets/IMG_5883 16.jpg'
 import img4 from './assets/walkingguy16.png'
-
+import Card from './components/Card';
 
 const App = () => {
   const images = [
@@ -19,7 +19,28 @@ const App = () => {
     <div>
       <Nav />
       <Carousel images={images} />
-      {/* about Family, School, Tennis, Everything Else */}
+      <div className="App">
+        <div className="card-container">
+          <div className="card-row">
+            <Card
+              title="Family"
+              content=""
+              imageUrl={img1} 
+            />
+            <Card
+              title="School"
+              content=""
+              imageUrl={img2} 
+            />
+            <Card
+              title="Others"
+              content=""
+              imageUrl={img3} 
+            />
+          </div>
+        </div>
+        {/* about Family, School, Tennis, Everything Else */}
+      </div>
     </div>
   );
 };
---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './Card.css';
+
+const Card = ({ title, content, imageUrl }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleCardClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div className={`card ${expanded ? 'expanded' : ''}`} onClick={handleCardClick}>
+      <div className="card-image">
+        <img src={imageUrl} alt={title} />
+      </div>
+      <div className="card-content">
+        <h2>{title}</h2>
+        {expanded && <p>{content}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
